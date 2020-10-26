8ced52008cc612da5fc95ae55b8a7200dd6e962f
Duncan Nixon <duncannixon@me.com>
2020-10-26 17:28:33 +0000

    Added unit test, updated dependencies.

---
+++ test/routes/epr-redirect.route.test.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const server = require('../../src/server')
+const TestHelper = require('../utilities/test-helper')
+
+describe('ePR Redirect route', () => {
+  const url = '/epr-redirect'
+
+  beforeAll((done) => {
+    server.events.on('start', () => {
+      done()
+    })
+  })
+
+  afterAll((done) => {
+    jest.clearAllMocks()
+
+    server.events.on('stop', () => {
+      done()
+    })
+    server.stop()
+  })
+
+  describe('GET:', () => {
+    const getOptions = {
+      method: 'GET',
+      url
+    }
+
+    it('should redirect to ePR', async () => {
+      await TestHelper.submitGetRequest(server, getOptions, 302)
+    })
+  })
+})
