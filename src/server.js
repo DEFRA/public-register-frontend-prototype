69b418457011173eb3c533b1e9bed988751e9db8
Duncan Nixon <duncannixon@users.noreply.github.com>
2020-10-30 10:50:04 +0000

    Added basic authentication
    This change adds basic authentication to the application, to prevent unauthorised access of environments during development and testing.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const Bcrypt = require('bcrypt')
+
 const config = require('./config/config')
 const { logger } = require('defra-logging-facade')
 const appVersion = require('../package.json').version
@@ -10,7 +12,31 @@ const server = Hapi.server({
   host: config.applicationUrl
 })
 
+const users = {
+  defra: {
+    username: 'defra',
+    password: '$2a$04$cIFq2TKhfYzmccljQdnzp.57V8Az61o7SUIIfUK.Bioc49q7YhlLm'
+  }
+}
+
+const validate = async (request, username, password) => {
+  const user = users[username]
+  if (!user) {
+    return { credentials: null, isValid: false }
+  }
+
+  const isValid = await Bcrypt.compare(password, user.password)
+  const credentials = { id: user.id, name: user.name }
+
+  return { isValid, credentials }
+}
+
 const init = async () => {
+  await server.register(require('@hapi/basic'))
+
+  server.auth.strategy('simple', 'basic', { validate })
+  server.auth.default('simple')
+
   await server.register(require('./plugins/back-link.plugin'))
   await server.register(require('./plugins/blipp.plugin'))
   // await server.register(require('./plugins/csrf-crumb.plugin'))
