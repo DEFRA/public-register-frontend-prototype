19a987802c7ef9150ab2791684956e344c16c3af
Duncan Nixon <duncannixon@users.noreply.github.com>
2021-02-18 15:16:24 +0000

    Reinstate basic authentication
    Reinstated basic authentication and added mechanism to turn on / off based on application configuration.

--- src/config/config.js
+++ src/config/config.js
@@ -35,3 +35,6 @@ config.ncccEmail = process.env.NCCC_EMAIL || 'NCCC_EMAIL'
 config.ncccEmailTemplateId = process.env.NCCC_EMAIL_TEMPLATE_ID || 'NCCC_EMAIL_TEMPLATE_ID'
 config.customerEmailTemplateId = process.env.CUSTOMER_EMAIL_TEMPLATE_ID || 'CUSTOMER_EMAIL_TEMPLATE_ID'
 config.informationRequestTimescale = parseInt(process.env.INFORMATION_REQUEST_TIMESCALE) || 20
+
+config.useBasicAuth = getBoolean(process.env.USE_BASIC_AUTH || false)
+config.basicAuthPassword = process.env.BASIC_AUTH_PASSWORD
